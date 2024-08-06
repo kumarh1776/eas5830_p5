2611cbb2401f6e78c5e3ae85edc898ee694f49a1
kumarh1776 <59850401+kumarh1776@users.noreply.github.com>
2024-08-06 10:33:58 -0400

    Add files via upload

---
+++ bridge.py
@@ -0,0 +1,73 @@
+from web3 import Web3
+from web3.contract import Contract
+from web3.providers.rpc import HTTPProvider
+from web3.middleware import geth_poa_middleware
+import json
+import sys
+from pathlib import Path
+
+source_chain = 'avax'
+destination_chain = 'bsc'
+contract_info = "contract_info.json"
+
+def connectTo(chain):
+    if chain == 'avax':
+        api_url = "https://api.avax-test.network/ext/bc/C/rpc"
+    elif chain == 'bsc':
+        api_url = "https://data-seed-prebsc-1-s1.binance.org:8545/"
+    
+    w3 = Web3(Web3.HTTPProvider(api_url))
+    w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+    return w3
+
+def getContractInfo(chain):
+    p = Path(__file__).with_name(contract_info)
+    try:
+        with p.open('r')  as f:
+            contracts = json.load(f)
+    except Exception as e:
+        print("Failed to read contract info")
+        print("Please contact your instructor")
+        print(e)
+        sys.exit(1)
+
+    return contracts[chain]
+
+def scanBlocks(chain):
+    if chain not in ['source', 'destination']:
+        print(f"Invalid chain: {chain}")
+        return
+
+    contracts = getContractInfo(chain)
+    w3 = connectTo(chain)
+
+    contract = w3.eth.contract(address=contracts['address'], abi=contracts['abi'])
+
+    latest_block = w3.eth.block_number
+    for block_num in range(latest_block - 5, latest_block + 1):
+        block = w3.eth.get_block(block_num, full_transactions=True)
+        for tx in block.transactions:
+            receipt = w3.eth.get_transaction_receipt(tx.hash)
+            logs = contract.events.Deposit().processReceipt(receipt)
+            for log in logs:
+
+                destination_contracts = getContractInfo('destination')
+                destination_w3 = connectTo('destination')
+                destination_contract = destination_w3.eth.contract(
+                    address=destination_contracts['address'],
+                    abi=destination_contracts['abi']
+                )
+                destination_contract.functions.wrap().transact({'from': log['args']['sender']})
+                print(f"Deposit event found in block {block_num}, called wrap()")
+
+            logs = contract.events.Unwrap().processReceipt(receipt)
+            for log in logs:
+
+                source_contracts = getContractInfo('source')
+                source_w3 = connectTo('source')
+                source_contract = source_w3.eth.contract(
+                    address=source_contracts['address'],
+                    abi=source_contracts['abi']
+                )
+                source_contract.functions.withdraw().transact({'from': log['args']['sender']})
+                print(f"Unwrap event found in block {block_num}, called withdraw()")
